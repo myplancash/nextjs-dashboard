dfa98692d1cd23edee47c1fd7f202a2d36ec855e
Sergio Torres <sergiotorres@Sergios-MacBook-Pro-M2.local>
2023-11-26 18:42:59 -0500

    TypeScript complaining that it expects an alt property in the Image component

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={invoice.name ? `${invoice.name}'s profile picture` : 'Profile picture'}
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -81,7 +81,7 @@ export default async function InvoicesTable({
               {invoices?.map((invoice) => (
                 <tr
                   key={invoice.id}
-                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                  className="w-full border-b py-3 text-sm last-of-type:border-none &:first-child>td:first-child:rounded-tl-lg &:first-child>td:last-child:rounded-tr-lg &:last-child>td:first-child:rounded-bl-lg &:last-child>td:last-child:rounded-br-lg"
                 >
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
@@ -90,7 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt='some description here'
+                        alt={invoice.name ? `${invoice.name}'s profile picture` : 'Profile picture'}
                       />
                       <p>{invoice.name}</p>
                     </div>
